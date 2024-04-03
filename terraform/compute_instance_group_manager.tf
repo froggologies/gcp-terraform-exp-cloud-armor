1efb30945cb391763e2cd74e604afd04fa7d886d
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-04-03 12:32:34 +0700

    feat(terraform): add compute instance group manager and instance template for juice-shop
    - Add compute instance group manager to manage a group of instances for juice-shop
    - Set the target size of the group to 1
    - Set the base instance name to "juice-shop"
    - Set the zone to "us-central1-f"
    
    feat(terraform): add compute instance template for juice-shop
    
    - Create a compute instance template for juice-shop
    - Set the machine type to "e2-micro"
    - Set the disk size to 10GB with pd-standard disk type
    - Set the source image to "cos-cloud/cos-stable"
    - Set the network and subnetwork to default and us-central1 respectively
    - Set the network tier to "STANDARD"
    - Set the restart policy to "Always"
    - Set the container image to "docker.io/bkimminich/juice-shop"
    - Set the service account email and scopes for cloud-platform access

---
+++ terraform/compute_instance_group_manager.tf
@@ -0,0 +1,12 @@
+resource "google_compute_instance_group_manager" "instance-group-manager-1" {
+  version {
+    name              = "primary"
+    instance_template = google_compute_instance_template.instance-template-1.id
+  }
+
+  project            = google_project.main_project.project_id
+  name               = "juice-shop-managed-group"
+  base_instance_name = "juice-shop"
+  zone               = "us-central1-f"
+  target_size        = "1"
+}
---
+++ terraform/compute_instance_template.tf
@@ -0,0 +1,47 @@
+resource "google_compute_instance_template" "instance-template-1" {
+  project      = google_project.main_project.project_id
+  name         = "juice-shop-template"
+  machine_type = "e2-micro"
+
+  tags = ["web"]
+
+  disk {
+    source_image = "cos-cloud/cos-stable"
+    auto_delete  = true
+    boot         = true
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+  }
+
+  network_interface {
+    network    = google_compute_network.default.id
+    subnetwork = google_compute_subnetwork.us-central1.id
+    access_config {
+      network_tier = "STANDARD"
+    }
+  }
+
+  scheduling {
+    preemptible       = false
+    automatic_restart = true
+  }
+
+  metadata = {
+    gce-container-declaration = <<-EOF
+    spec:
+      containers:
+        - name: juice-shop
+          image: docker.io/bkimminich/juice-shop
+          stdin: false
+          tty: false
+      restartPolicy: Always
+      # This container declaration format is not public API and may change without notice. Please
+      # use gcloud command-line tool or Google Cloud Console to run Containers on Google Compute Engine.
+    EOF
+  }
+
+  service_account {
+    email  = google_service_account.instance-1.email
+    scopes = ["cloud-platform"]
+  }
+}
