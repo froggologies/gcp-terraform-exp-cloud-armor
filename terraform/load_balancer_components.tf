fc9719fd521bd57021999f6b3ec56bb94746e6eb
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-04-18 11:30:58 +0700

    fix(load_balancer_components.tf): rename health check resource from "tcp-port-3000" to "health-check1" for better clarity fix(load_balancer_components.tf): change port specification for health check from "USE_FIXED_PORT" to "USE_SERVING_PORT" to align with best practices fix(load_balancer_components.tf): update backend service health check reference from "google_compute_health_check.tcp-port-3000.id" to "google_compute_health_check.health-check1.id" to reflect the renamed health check resource fix(load_balancer_components.tf): change port range for global forwarding rule from "3000" to "80" to match the serving port of the backend service

--- terraform/load_balancer_components.tf
+++ terraform/load_balancer_components.tf
@@ -1,11 +1,10 @@
-resource "google_compute_health_check" "tcp-port-3000" {
+resource "google_compute_health_check" "health-check1" {
   depends_on = [google_project_service.services.0]
   project    = google_project.main_project.project_id
-  name       = "tcp-port-3000"
+  name       = "health-check1"
 
-  tcp_health_check {
-    port_specification = "USE_FIXED_PORT"
-    port               = 3000
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
   }
   log_config {
     enable = true
@@ -18,11 +17,13 @@ resource "google_compute_backend_service" "backend-service" {
   project       = google_project.main_project.project_id
   protocol      = "HTTP"
   name          = "backend-service"
-  health_checks = [google_compute_health_check.tcp-port-3000.id]
+  health_checks = [google_compute_health_check.health-check1.id]
   port_name     = "http"
 
   backend {
-    group = google_compute_instance_group_manager.instance-group-manager-1.instance_group
+    group           = google_compute_instance_group_manager.instance-group-manager-1.instance_group
+    balancing_mode  = "UTILIZATION"
+    max_utilization = 0.8
   }
   log_config {
     enable = true
@@ -45,6 +46,6 @@ resource "google_compute_target_http_proxy" "juice-shop-proxy" {
 resource "google_compute_global_forwarding_rule" "juice-shop-forwarding-rule" {
   project    = google_project.main_project.project_id
   name       = "juice-shop-forwarding-rule"
-  port_range = "3000"
+  port_range = "80"
   target     = google_compute_target_http_proxy.juice-shop-proxy.id
 }
